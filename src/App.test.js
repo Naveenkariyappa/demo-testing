4fa6992613ade3bf7a292c4d40e07c2a8e280adc
Naveen Kariyappa <naveenkariyappa@gmail.com>
2020-12-31 09:18:03 +0530

    Before Ejecting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+})
+
+test('No Errors on render', () => {
+  const wrapper = shallow(<App/>)
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1)
+});
+
+test('Renders a button', () => {
+  
+});
+
+test('Renders a counter', () => {
+  
+});
+
+test('counter starts at 0', () => {
+  
+});
+
+test('Clicking on button increments the count', () => {
+  
 });
